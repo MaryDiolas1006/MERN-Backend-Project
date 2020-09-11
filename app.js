73fc68edad56145b9dd75262b08dc20d32d89d84
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-09-11 10:49:34 +0800

    added app.js file

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const cors = require('cors');
+
+// initialize the application using express
+const app = express();
+
+// declare what port will be use by the application
+const port = process.env.PORT || 5000
+
+
+const movies = require('./routes/movie');
+
+
+
+// connect to database using mongoose
+mongoose.connect('mongodb://localhost:27017/movies', {
+// // mongoose.connect(process.env.ATLAS, {
+	useNewUrlParser: true, 
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true
+});
+
+
+// // Route Middleware
+
+// // application level middleware
+app.use((req, res, next) => {
+	// this will show what method is requesting ex:post, put delete and get
+	console.log(req.method);
+	next();
+});
+
+
+// // parse incoming request body to json format
+// // this will create body attribute to request object
+app.use(express.json());
+// app.use(cors());
+
+// // serve a static file
+// // app.use('/public', express.static('assets/images'))
+
+app.use('/movies', movies);
+
+
+
+app.get('/', (req, res,) => {
+	res.send({
+		message: "App is running"
+	})
+});
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+	res.status(400).send({
+		error: err.message
+	})
+});
+
+
+// this will run the application on the desired port
+app.listen(port, () => {
+	console.log(`App is listening at port ${port}`)
+});
